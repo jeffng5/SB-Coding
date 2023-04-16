79771b57d61e6754dd6a646b7b293c999697fe69
Jeffrey Ng <jefreykng@hotmail.com>
2023-04-16 19:21:15 -0400

    can post multiple memes in one page

--- memes.js
+++ memes.js
@@ -1,39 +1,3 @@
-
-// function add(){
-
-// const add = document.querySelector('#user-input').value 
-// const h2 = document.querySelector('h2')
-// h2.append(add)
-// return h2}
-
-// const uppertext = document.getElementById("topbutton")
-// uppertext.addEventListener("click", function(e) {add() ; e.preventDefault()})
-
-// function addPunchLine(){
-// const punchLine= document.querySelector('#user-input2').value
-// const span = document.querySelector('span')
-// span.append(punchLine)
-// return span}
-
-// const bottomtext = document.getElementById("bottombutton")
-// bottomtext.addEventListener("click", function(e) {addPunchLine() ; e.preventDefault()})
-
-
-
-// //I used https://www.youtube.com/watch?v=lzK8vM_wdoY for this block of code//
-// const memegen = document.getElementById('photo')
-// let uploaded_image = ""
-
-// memegen.addEventListener("change", function(){
-// const reader = new FileReader();
-// reader.addEventListener("load", () => {
-//     uploaded_image = reader.result;
-//     document.querySelector('#display_image').style.backgroundImage = `url(${uploaded_image})`
-// });
-// reader.readAsDataURL(this.files[0]);
-
-// })
-
 function addPunchLine(){
     const add = document.querySelector('#user-input').value 
     const h2 = document.querySelector('h2')
@@ -47,50 +11,26 @@ const bottomtext = document.getElementById("bottombutton")
 bottomtext.addEventListener("click", function(e) {e.preventDefault(); addPunchLine()})
 
 
-//I used https://www.codewithlinda.com/blog/upload-picture-on-the-web for these block of code///
-
-const getValuesFromInputs = () =>{
-    const profileImg = document.querySelector('input.profile-img').files[0];
-    const profileVid = document.querySelector('input.profile-vid').files[0];
- 
-    document.querySelector('form').style.display = 'none';
- 
-    return [profileImg, profileVid];
- 
- }
-
-
- const convertInputValues = () => {
-    const [profileImg, profileVid] = getValuesFromInputs();
-  let binaryData=[]
-  let binaryData2=[]
-  binaryData.push(profileVid)
-  binaryData2.push(profileImg)
-    
-  const profileImgURL = URL.createObjectURL(new Blob(binaryData2, {type: "application/zip"}));
-  const profileVidURL = URL.createObjectURL(new Blob(binaryData, {type: "application/zip"}));  
-  
-    return[profileVidURL, profileImgURL]
-  }
-
-
-  const addInputToProfile = () => {
-
-    const [profileVidURL, profileImgURL]  = convertInputValues();
-
-    document.querySelector('.profile img').setAttribute('src', profileImgURL);
-    document.querySelector('.vid').setAttribute('src', profileVidURL);
-
+function uploadImage(){
+  let val = document.querySelector('#imagename').value
+  src= val;
+  img = document.querySelector('img');
+  img.src = src;
+  document.getElementById('display_image').appendChild(img);
+  let frame=document.getElementById('display_image');
+  let frame2= document.createElement('img');
+  frame.appendChild(frame2).setAttribute('src')
+  return frame2;
 }
 
 let meme=document.getElementById('bottombutton')
 meme.addEventListener('click', (e) => {
-    e.preventDefault();
-    addInputToProfile();
+    e.preventDefault(); uploadImage()
+
   });
 
 
-let delvid=document.getElementById('remobutton')
+let delvid=document.getElementById('rembutton')
 delvid.addEventListener('click', function(e) {
     let vidtag=document.getElementsByClassName('vid')
     let imgtag=document.getElementsByClassName('profile')
